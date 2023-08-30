6eed812e9057a1f7033f4e3ac6fa076957a80b56
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-08-30 11:19:15 +0300

    JS

---
+++ if-else/jscript.js
@@ -0,0 +1,27 @@
+/*            **IF-Else yapısı          */
+
+/* if (koşul) {
+    bunu yap
+} else {
+
+    koşulu sağlamazsa bunu yap
+
+}
+*/
+
+
+
+
+    let arin = false;
+
+    
+
+
+if (arin === true){
+
+    document.write('arin ifadesi doğrudur.');
+
+} else {
+    document.write('arin ifadesi yanlıştır.');
+}
+
