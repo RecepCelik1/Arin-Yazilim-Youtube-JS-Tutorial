c4defe082adcfce6ad65861951a30b9fce756d5d
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-08-30 11:16:46 +0300

    JS

---
+++ değiskenler/jscript.js
@@ -0,0 +1,8 @@
+var firstName, secondName, fullName;
+
+firstName = prompt("Adınız Nedir ?");
+secondName = prompt("Soyadınız Nedir?");
+
+fullName = firstName + ' ' + secondName;
+
+document.write("Merhaba, " + fullName);
